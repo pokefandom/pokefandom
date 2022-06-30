2b9bad1a9c58eff5988a613ef0637cea3e37eece
billybolton <billybolton16@gmail.com>
2022-06-30 02:16:06 -0400

    font page for game

---
+++ assets/js/trivia/trivia-welcome.js
@@ -0,0 +1,29 @@
+let gameType = "";
+
+async function loadGamePage(){
+    
+        let container = document.getElementById("init-container");
+    container.style.display = "block";
+    
+        let progress = document.getElementById("progress");
+    progress.style.display = "none";
+    
+    let gameDiv = document.getElementById("game-div");
+    gameDiv.style.display = "none";
+}
+
+async function setupGame(id){
+    console.log("clicked");
+    gameType = id.split("-")[0];
+    
+    let container = document.getElementById("init-container");
+    container.style.display = "none";
+    
+    let progress = document.getElementById("progress");
+    progress.style.display = "block";
+    
+    let gameDiv = document.getElementById("game-div");
+    gameDiv.style.display = "block";
+    
+    await setRound();
+}
